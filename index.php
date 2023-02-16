93c03c1e9449d7a73652fa167c42196882ee8c5c
Donjatas <xdonatasx.l@gmail.com>
2023-02-16 14:05:11 +0200

    Registracija ir Prisijungimas

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['email'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+
+
+    // Connect to the database
+	include 'dbconnect.php';
+  
+	$email = $_SESSION['email'];
+	// Query to retrieve information from the database
+	$query = "SELECT * FROM user WHERE email='$email'";
+	$result = mysqli_query($db, $query);
+	$user = mysqli_fetch_assoc($result);
+	
+	// Assign the retrieved information to session variables
+	$_SESSION['first_Name'] = $user['First_Name'];
+	$_SESSION['last_Name'] = $user['Last_Name'];
+
+
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['email']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['email'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['first_Name']; echo " "; echo $_SESSION['last_Name']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+		
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+// initializing variables
+$first_Name = "";
+$last_Name = "";
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'socialmedia');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $first_Name = mysqli_real_escape_string($db, $_POST['first_Name']);
+  $last_Name = mysqli_real_escape_string($db, $_POST['last_Name']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($first_Name)) { array_push($errors, "First name is required"); }
+  if (empty($last_Name)) { array_push($errors, "Last name is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) { array_push($errors, "The two passwords do not match"); }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same first_Name and/or email
+  $user_check_query = "SELECT email FROM user where email = '$email'";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+
+    if(!empty($user)) {
+      array_push($errors, "Email already exists");
+    }
+
+
+
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO user (first_Name, last_Name, email, password) 
+  			  VALUES('$first_Name', '$last_Name', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['first_Name'] = $first_Name;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+  
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+  
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['email'] = $email;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: index.php');
+        }else {
+            array_push($errors, "Wrong email/password combination");
+        }
+    }
+  }
+  
+  ?>
